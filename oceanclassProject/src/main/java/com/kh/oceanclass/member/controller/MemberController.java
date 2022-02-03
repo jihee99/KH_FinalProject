95ec04129adeca3fbae21d5f8c1b246d62323113
su31n <95194725+su31n@users.noreply.github.com>
2022-02-03 21:31:32 +0900

    <docs> MemberController 추가
    - add com.kh.oceanclass.member.controller.MemberController

--- oceanclassProject/src/main/java/com/kh/oceanclass/member/controller/MemberController.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/member/controller/MemberController.java
@@ -1,5 +1,15 @@
 package com.kh.oceanclass.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
 
+	@RequestMapping(value="classEnrollManager.ad")
+	public String adminClassEnrollManager() {
+		// 관리자 마이페이지 이동용(뷰 확인용) 컨트롤러
+		return "member/admin/adminClassEnrollManager";
+	}
+	
 }
