6eacff32ebec6026d9e63695829cc4381af0f17a
su31n <95194725+su31n@users.noreply.github.com>
2022-02-04 00:29:37 +0900

    <etc> 주석 변경
    - correct com.kh.oceanclass.member.MemberController

--- oceanclassProject/src/main/java/com/kh/oceanclass/member/controller/MemberController.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/member/controller/MemberController.java
@@ -8,7 +8,7 @@ public class MemberController {
 
 	@RequestMapping(value="classEnrollManager.ad")
 	public String adminClassEnrollManager() {
-		// 관리자 마이페이지 이동용(뷰 확인용) 컨트롤러
+		// 관리자 클래스 등록 관리 페이지 이동용(뷰 확인용) 메소드
 		return "member/admin/adminClassEnrollManager";
 	}
 	
