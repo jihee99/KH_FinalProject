07c5ecac3a24ee57989f8ce2ad2e13777e03b587
jihee <oosoojh31@gmail.com>
2022-02-07 22:55:26 +0900

    Update InstructorStoreController.java

--- oceanclassProject/src/main/java/com/kh/oceanclass/store/controller/InstructorStoreController.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/store/controller/InstructorStoreController.java
@@ -2,17 +2,20 @@ package com.kh.oceanclass.store.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.oceanclass.common.model.vo.PageInfo;
 import com.kh.oceanclass.common.template.Pagination;
 import com.kh.oceanclass.store.model.service.InstructorStoreService;
 import com.kh.oceanclass.store.model.vo.Product;
+import com.kh.oceanclass.store.model.vo.ProductOption;
 
 /*강사 스토어 관련 기능 처리하는 controller*/
 
@@ -41,6 +44,14 @@ public class InstructorStoreController {
 	public String enrollForm() {
 		return "store/instructorStoreEnrollPage";
 	}
+	
+	@RequestMapping(value="stenroll.in", produces="application/json; charset=UTF-8")
+	public void insertProduct(Product p, ProductOption op, MultipartFile[] upfile, HttpSession session, Model model) {
+		System.out.println(p);
+		System.out.println(op); 
+		System.out.println(upfile);
+	}
+	
 	
 	@RequestMapping("stdetail.in")
 	public String selectProduct(int pno, Model model) {
