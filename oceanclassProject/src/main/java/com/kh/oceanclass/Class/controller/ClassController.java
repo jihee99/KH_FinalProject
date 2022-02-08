d301e1a4d481450cad3855e1018d60eb08cdebaa
su31n <95194725+su31n@users.noreply.github.com>
2022-02-09 00:53:24 +0900

    <docs> class controller method 추가
    - add code to Class.controller.ClassController

--- oceanclassProject/src/main/java/com/kh/oceanclass/Class/controller/ClassController.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/Class/controller/ClassController.java
@@ -14,4 +14,28 @@ public class ClassController {
 		return "class/classDetail";
 	}
 	
+	@RequestMapping(value="classPay.me")
+	public String classPay() {
+		// 클래스 결제 페이지 이동용(뷰 확인용) 메소드
+		return "class/classPay";
+	}
+	
+	@RequestMapping(value="classPayComplate.me")
+	public String classPayComplate() {
+		// 클래스 결제 완료 페이지 이동용(뷰 확인용) 메소드
+		return "class/classPayComplate";
+	}
+	
+	@RequestMapping(value="classReviewList.me")
+	public String classReviewList() {
+		// 클래스 리뷰 리스트 이동용(뷰 확인용) 메소드
+		return "class/classReviewList";
+	}
+	
+	@RequestMapping(value="classReviewDetail.me")
+	public String classReviewDetail() {
+		// 클래스 리뷰 디테일 이동용(뷰 확인용) 메소드
+		return "class/classReviewDetail";
+	}
+	
 }
