351dfc6648095042c29b73e3589a54fa3d5bfd4c
su31n <95194725+su31n@users.noreply.github.com>
2022-02-10 17:38:16 +0900

    <feat> 클래스 상세 보기 및 조회수 기능 추가
    - add Class.controller.ClassController (classDatail method)

--- oceanclassProject/src/main/java/com/kh/oceanclass/Class/controller/ClassController.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/Class/controller/ClassController.java
@@ -1,17 +1,39 @@
 package com.kh.oceanclass.Class.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.oceanclass.Class.model.service.ClassServiceImpl;
+import com.kh.oceanclass.Class.model.vo.ClassVo;
 
 /*사용자 클래스 관련 기능 처리하는 controller*/
 
 @Controller
 public class ClassController {
 
+	@Autowired
+	private ClassServiceImpl cService;
+	
 	@RequestMapping(value="classDetail.me")
-	public String classDetail() {
-		// 클래스 상세보기 페이지 이동용(뷰 확인용) 메소드
-		return "class/classDetail";
+	public String classDetail(int clNo, Model model, HttpSession session) {
+		// 클래스 상세보기 페이지
+		
+		int result = cService.increaseCount(clNo);
+		
+		if(result > 0) {
+			// 조회수 증가 성공 -> 정상적인 페이지 -> 내용 넣어서 상세보기 해오기
+			ClassVo c = cService.selectClass(clNo);
+			model.addAttribute("c", c);
+			return "class/classDetail";
+		} else {
+			session.setAttribute("alertMsg", "조회할 수 없는 클래스입니다.");
+			return "redirect:/";
+		}
+		
 	}
 	
 	@RequestMapping(value="classPay.me")
