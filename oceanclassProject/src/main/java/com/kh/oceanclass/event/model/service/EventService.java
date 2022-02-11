33d016a5476ec4073031b2769086d77fb54a19f2
jjay0303 <jybae0601@gmail.com>
2022-02-12 01:25:43 +0900

    <update> event 게시판 구현중
    - add Event.java
    - add EventController.java
    - add EventService.java
    - add EventServiceImpl.java
    - add EventDao.java
    - add event-mapper.xml
    - add eventMain.jsp / css

--- oceanclassProject/src/main/java/com/kh/oceanclass/event/model/service/EventService.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/event/model/service/EventService.java
@@ -1,5 +1,23 @@
 package com.kh.oceanclass.event.model.service;
 
-public class EventService {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+
+import com.kh.oceanclass.common.model.vo.PageInfo;
+import com.kh.oceanclass.event.model.vo.Event;
+
+@Service
+public interface EventService {
+	
+	// 1. 이벤트 리스트
+	int selectEventCount();
+	ArrayList<Event> selectEventList(PageInfo pi);
+	
+	// 2. 이벤트 상세보기
+	Event selectEvent(int eventNo);
+	
+	// 3. 이벤트 카테고리 선택
+	ArrayList<Event> ajaxSearchEvent(String category);
+	
 }
