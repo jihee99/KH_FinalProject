d3f48810db0c113d6591e2bdb22e791d4125923e
jjay0303 <jybae0601@gmail.com>
2022-02-13 00:00:42 +0900

    <docs> Comment 객체 생성
    - Comment vo 객체 생성

---
+++ oceanclassProject/src/main/java/com/kh/oceanclass/common/model/vo/Comment.java
@@ -0,0 +1,21 @@
+package com.kh.oceanclass.common.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Comment {
+	
+	public int commentNo;	// 댓글고유번호
+	public int memNo;		// 회원번호
+	public int boardNo;		// 게시판분류 1.이벤트 2.클래스리뷰
+	public int contentNo;	// 게시글 고유번호
+	public String replyContent;	
+	public String replyDate;
+	public String replyStatus;
+}
