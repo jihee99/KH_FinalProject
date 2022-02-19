d06a53192b1b55f1be839968bdf0cfa20a6b9308
jjay0303 <jybae0601@gmail.com>
2022-02-20 04:11:35 +0900

    <etc> CsQna 객체 추가
    - CsQna 객체 추가
    - mybatis-config에 typealias 추가

---
+++ oceanclassProject/src/main/java/com/kh/oceanclass/common/model/vo/CsQna.java
@@ -0,0 +1,29 @@
+package com.kh.oceanclass.common.model.vo;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class CsQna {
+	
+	private int csQno;
+	private int memNo;
+	private int gbNo;
+	private int referNo;
+	private String title;
+	private String content;
+	private Date createDate;
+	private int pwd;
+	private String filePath;
+	private String status;
+	private String answerContent;
+	private Date answerDate;
+	
+}
