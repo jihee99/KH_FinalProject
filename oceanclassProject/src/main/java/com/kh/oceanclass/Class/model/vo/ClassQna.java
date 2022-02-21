411342ef3f4891b9988b7a6e004e4a6d65c28d8d
su31n <95194725+su31n@users.noreply.github.com>
2022-02-22 03:08:10 +0900

    <docs> ClassQna vo클래스 추가
    - add Class.model.vo.ClassQna

---
+++ oceanclassProject/src/main/java/com/kh/oceanclass/Class/model/vo/ClassQna.java
@@ -0,0 +1,29 @@
+package com.kh.oceanclass.Class.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ClassQna {
+
+	private int csQnaNo;
+	private String memNo; // 리스트 불러올때는 작성자 닉네임으로 저장
+	private int gbNo;
+	private int referNo;
+	private String title;
+	private String content;
+	private String createDate;
+	private Integer pwd;
+	private String filePath;
+	private String status;
+	private String answerContent;
+	private String answerDate;
+	
+	private String filePathMessage;
+	
+}
