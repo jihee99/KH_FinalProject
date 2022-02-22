913e97d912005476500e6fbc067f76f50c598751
kimhyesun719 <89463167+kimhyesun719@users.noreply.github.com>
2022-02-23 01:18:38 +0900

    <update> 강사 클래스 컨트롤러 미완성 부분 삭제
    오류 방지

--- oceanclassProject/src/main/java/com/kh/oceanclass/Class/model/service/TcClassServiceImpl.java
+++ oceanclassProject/src/main/java/com/kh/oceanclass/Class/model/service/TcClassServiceImpl.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TcClassServiceImpl implements TcClassService {
 
+	@Override
+	public int myClassList(int clNo) {
+		return 0;
+	}
+
 	
 	
 }
